ba6b2f99d044dae7cb3e158685c44c2acd238762
Leo Yang <qa894178522@qq.com>
2026-05-06 19:40:21 +0800

    wip: add missing using directives
    Add required using statements to fix compilation and enable intended functionality:
    - PrinterApi.cs: add Newtonsoft.Json.Serialization for JSON contract/serializer usage.
    - PdfRenderService.cs: add System.IO for file/stream operations.
    - PrinterService.cs: add System for core system types.
    These imports resolve missing symbol errors and allow the services to use serialization and IO features as intended.

--- lib/EasyInk.Net/EasyInk.Printer/src/PrinterApi.cs
+++ lib/EasyInk.Net/EasyInk.Printer/src/PrinterApi.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using EasyInk.Printer.Models;
 using EasyInk.Printer.Services;
 using EasyInk.Printer.Services.Abstractions;
--- lib/EasyInk.Net/EasyInk.Printer/src/Services/PdfRenderService.cs
+++ lib/EasyInk.Net/EasyInk.Printer/src/Services/PdfRenderService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using EasyInk.Printer.Models;
 using EasyInk.Printer.Services.Abstractions;
 using PdfiumViewer;
